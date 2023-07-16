04ece3e9143a27c2fc34e59269df330ea9288f39
wangchunguang <1914426389@qq.com>
2023-07-16 17:24:37 +0800

    feat: add deps optimizer

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-logo" src={logo} alt="" />
+        {/* <img className="App-logo" src={logo} alt="" /> */}
         <p>Hello Vite + React</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
---
+++ src/node/optimizer/scanPlugin.ts
@@ -0,0 +1,38 @@
+import { Plugin } from 'esbuild'
+
+import { BARE_IMPORT_RE, EXTERNAL_TYPES} from '../constants'
+
+export function scanPlugin(deps: Set<string>): Plugin {
+    return {
+        name:' esbuild:scan-deps',
+        setup(build) {
+
+            // 忽略的 文件类型
+            build.onResolve(
+                { filter: new RegExp(`\\.${EXTERNAL_TYPES.join('|')}$`) },
+                (resolveInfo) => {
+                    return {
+                        path: resolveInfo.path,
+                        // 打上 external 标签
+                        external: true
+                    }
+                }
+            )
+
+            build.onResolve(
+                { filter: BARE_IMPORT_RE },
+                (resolveInfo) => {
+                    const { path: id } = resolveInfo
+
+                    // 推入 deps 集合中
+                    deps.add(id)
+
+                    return {
+                        path: id,
+                        external: true
+                    }
+                }
+            )
+        }
+    }
+}
