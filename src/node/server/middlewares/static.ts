27d634f6a6c24253148cc6794d176c2a54b5a890
wangchunguang <1914426389@qq.com>
2023-07-17 01:35:56 +0800

    feat: add static middleware

---
+++ src/node/server/middlewares/static.ts
@@ -0,0 +1,18 @@
+import { NextHandleFunction } from "connect"
+import sirv from "sirv"
+
+import { CLIENT_PUBLIC_PATH } from "../../constants"
+import { isImportRequest } from "../../utils"
+
+export function staticMiddleware(root: string): NextHandleFunction {
+  const serveFromRoot = sirv(root, { dev: true });
+  return async (req, res, next) => {
+    if (!req.url) {
+      return
+    }
+    if (isImportRequest(req.url) || req.url === CLIENT_PUBLIC_PATH) {
+      return
+    }
+    serveFromRoot(req, res, next)
+  }
+}
