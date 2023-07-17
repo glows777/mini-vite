5a8399235c8ec0d5244f8451c3766ff6336abeb6
wangchunguang <1914426389@qq.com>
2023-07-17 13:17:52 +0800

    fix: esbuild pre bundle entry error

--- src/node/plugins/index.ts
+++ src/node/plugins/index.ts
@@ -1,12 +1,16 @@
+import { Plugin } from '../plugin'
 import { resolvePlugin } from './resolve'
 import { esbuildTransformPlugin  } from './esbuild'
 import { importAnalysisPlugin } from './importAnalysis'
-import { Plugin } from '../plugin'
+import { cssPlugin } from './css'
+import { assertPlugin } from './assert'
 
 export function resolvePlugins(): Plugin[] {
     return [
         resolvePlugin(),
         esbuildTransformPlugin(), 
-        importAnalysisPlugin()
+        importAnalysisPlugin(),
+        cssPlugin(),
+        assertPlugin()
     ]
 }
