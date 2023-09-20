ff0746e5031051c660768d016ca1ce081520995c
glows777 <glows778@gmail.com>
2023-09-20 13:37:43 +0800

    fix: macOS auto request favicon

--- src/node/server/middlewares/static.ts
+++ src/node/server/middlewares/static.ts
@@ -15,7 +15,11 @@ export function staticMiddleware(root: string): NextHandleFunction {
     if (isImportRequest(req.url) || req.url === CLIENT_PUBLIC_PATH)
       return
 
-    const url = decodeURIComponent(cleanUrl(req.url))
+    let url = path.join(root, decodeURIComponent(cleanUrl(req.url)))
+    // todo mac 环境下 会自动请求 favicon icon ，所以这里暂时硬编码一下
+    if (url.includes('favicon.ico'))
+      url = path.join(root, 'public/favicon.svg')
+
     const ext = path.extname(url)
     const contentType = mime.getType(ext)
 
